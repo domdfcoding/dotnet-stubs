38ac95ee67b322d617a6f339a81200ba5b5cd0b0
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2024-10-29 11:41:16 +0000

    Configuration update

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@
 # This file is managed by 'repo_helper'. Don't edit it directly.
 
 # stdlib
+import pathlib
+import shutil
 import sys
 
 # 3rd party
@@ -12,13 +14,16 @@ sys.path.append('.')
 # this package
 from __pkginfo__ import *  # pylint: disable=wildcard-import
 
-
+repo_root = pathlib.Path(__file__).parent
+install_requires = (repo_root / "requirements.txt").read_text(encoding="UTF-8").split('\n')
 
 setup(
-		description='Incomplete, and probably incorrect, stubs for .NET.',
+		description="Incomplete, and probably incorrect, stubs for .NET",
 		extras_require=extras_require,
 		install_requires=install_requires,
+		name="dotnet-stubs",
 		py_modules=[],
-		version=__version__,
 		packages=["System-stubs", "dotnet_stub_builder"]
 		)
+
+shutil.rmtree("dotnet_stubs.egg-info", ignore_errors=True)
